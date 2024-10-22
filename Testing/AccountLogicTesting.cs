daa190fa50bc3c55cb43990b1f7d9ec602f37070
SoufianeOuldAmar <soufiane_ouldamar@outlook.com>
2024-10-23 01:01:23 +0200

    Fixed unit test errors because class wasnt static first

--- Testing/AccountLogicTesting.cs
+++ Testing/AccountLogicTesting.cs
@@ -5,10 +5,9 @@ public class TestAccountLogic
 {
     [TestMethod]
     public void TestLogin()
-    {   
-        AccountsLogic accountsLogic = new AccountsLogic();
-        var existingAccount = accountsLogic.CheckLogin("n@b.c", "xyz");
-        var nonExistentAccount = accountsLogic.CheckLogin("iemand@outlook.com", "password");
+    {
+        var existingAccount = AccountsLogic.CheckLogin("n@b.c", "xyz");
+        var nonExistentAccount = AccountsLogic.CheckLogin("iemand@outlook.com", "password");
 
         Assert.IsNotNull(existingAccount, "Account should not be null");
         Assert.IsNull(nonExistentAccount, "Account should be null");
@@ -16,33 +15,32 @@ public class TestAccountLogic
 
     [TestMethod]
     public void TestCreateAccount()
-    {   
-        AccountsLogic accountsLogic = new AccountsLogic();
+    {
 
         // Expected result for a successful account creation
         List<AccountsLogic.CreateAccountStatus> statusListEmpty = new List<AccountsLogic.CreateAccountStatus>();
-        var successfulAccount = accountsLogic.CheckCreateAccount("Soufiane Ould Amar", "soufiane_ouldamar@outlook.com", "password");
+        var successfulAccount = AccountsLogic.CheckCreateAccount("Soufiane Ould Amar", "soufiane_ouldamar@outlook.com", "password");
 
         // Expected result for an unsuccessful account creation due to incorrect full name
         List<AccountsLogic.CreateAccountStatus> statusListFullNameWrong = new List<AccountsLogic.CreateAccountStatus>
         {
             AccountsLogic.CreateAccountStatus.IncorrectFullName
         };
-        var unsuccessfulFullNameAccount = accountsLogic.CheckCreateAccount("123", "iemand_anders@outlook.com", "password");
+        var unsuccessfulFullNameAccount = AccountsLogic.CheckCreateAccount("123", "iemand_anders@outlook.com", "password");
 
         // Expected result for an unsuccessful account creation due to incorrect email
         List<AccountsLogic.CreateAccountStatus> statusListEmailWrong = new List<AccountsLogic.CreateAccountStatus>
         {
             AccountsLogic.CreateAccountStatus.IncorrectEmail
         };
-        var unsuccessfulEmailAccount = accountsLogic.CheckCreateAccount("Iemand", "iemand", "password");
+        var unsuccessfulEmailAccount = AccountsLogic.CheckCreateAccount("Iemand", "iemand", "password");
 
         // Expected result for an unsuccessful account creation due to incorrect password
         List<AccountsLogic.CreateAccountStatus> statusListPasswordWrong = new List<AccountsLogic.CreateAccountStatus>
         {
             AccountsLogic.CreateAccountStatus.IncorrectPassword
         };
-        var unsuccessfulPasswordAccount = accountsLogic.CheckCreateAccount("Iemand", "iemand@outlook.com", "pass");
+        var unsuccessfulPasswordAccount = AccountsLogic.CheckCreateAccount("Iemand", "iemand@outlook.com", "pass");
 
         // Use CollectionAssert to compare the contents of the lists
         CollectionAssert.AreEqual(statusListEmpty, successfulAccount, "statusListEmpty should be empty.");
